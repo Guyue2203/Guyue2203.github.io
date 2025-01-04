1ed8dac307d06fec93ebd13d12624bc034a62a02
Guyue2203 <2583526329@qq.com>
2025-01-04 17:42:39 +0800

    Site updated: 2025-01-04 17:42:38

---
+++ js/lazyload-bg.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var lazyBackgrounds = document.querySelectorAll('.lazyload-bg');
+    
+    if ('IntersectionObserver' in window) {
+        let lazyBackgroundObserver = new IntersectionObserver(function(entries, observer) {
+            entries.forEach(function(entry) {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add('loaded');
+                    lazyBackgroundObserver.unobserve(entry.target);
+                }
+            });
+        });
+
+        lazyBackgrounds.forEach(function(lazyBackground) {
+            lazyBackgroundObserver.observe(lazyBackground);
+        });
+    }
+});
